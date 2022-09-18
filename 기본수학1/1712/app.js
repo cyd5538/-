759acff6e0858d99419be01376dd8a830cd6276b
CYJ <91642972+cyd5538@users.noreply.github.com>
2022-09-18 21:08:01 +0900

    2022-09-18

---
+++ 기본수학1/1712/app.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+    
+input = input[0];
+input = input.split(' ').map((item) => +item);
+solution(input[0], input[1], input[2]);
+
+function solution(A, B, C) {
+    let N = A / (C - B);
+    let answer = Math.floor(N) + 1;
+    B >= C ? (answer = -1) : answer;
+    console.log(answer);
+    return answer
+}
