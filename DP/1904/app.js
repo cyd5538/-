b21470b38e9121f31f9af00a631f3086b83fd489
CYJ <91642972+cyd5538@users.noreply.github.com>
2023-03-05 21:39:14 +0900

    2023-03-05

---
+++ DP/1904/app.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim()
+
+let N = +input
+let arr = Array(N+1).fill(0)
+arr[1] = 1;
+arr[2] = 2;
+
+
+for(let i=3; i < arr.length; i++){
+  arr[i] = (arr[i-1] + arr[i-2]) % 15746
+}
+
+console.log(arr[N])
