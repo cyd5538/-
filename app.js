21393b42c8c0fff2f0349823ea0e956d66387fc1
cyj <cyd9911@gmail.com>
2023-04-16 10:32:59 +0900

    2023-04-16

--- app.js
+++
@@ -1,6 +0,0 @@
-const a = [
-    { N: 5, arr: [ 50, 50, 70, 80, 100 ] },
-]
-
-console.log(a[0].arr.reduce((a,b) => a+b)/a[0].N)
-
--- 정렬/8979/app.js
+++ 정렬/8979/app.js
@@ -7,6 +7,7 @@ const k = input[0].split(" ").map(Number)[1]
 
 let medalList = [];
 
+// 나라번호, 골드, 실버, 브론즈별로 갯수 넣어주고
 for (let i = 1; i < input.length; i++) {
   const [country, gold, silver, bronze] = input[i].split(" ").map(Number);
   medalList.push({
@@ -17,19 +18,20 @@ for (let i = 1; i < input.length; i++) {
   });
 }
 
+// 메달별로 정렬
 medalList.sort((a, b) => {
     if (a.gold !== b.gold) return b.gold - a.gold;
     if (a.silver !== b.silver) return b.silver - a.silver;
     return b.bronze - a.bronze;
 });
 
+// 초기 순위 1등을 넣어주고 2번째 인덱스가 1등이랑 메달수가 같으면 똑같은 순위로 ranks 배열에 넣어줌
 let rank = 1;
 let prevMedal = [medalList[0].gold, medalList[0].silver, medalList[0].bronze];
 let ranks = [1];
 
 for (let i = 1; i < medalList.length; i++) {
-  const currMedal = [medalList[i].gold, medalList[i].silver, medalList[i].bronze];
-
+  const currMedal = [medalList[i].gold  
   if (prevMedal[0] === currMedal[0] && prevMedal[1] === currMedal[1] && prevMedal[2] === currMedal[2]) {
     ranks.push(rank);
   } else {
