443896c8ee2a7fbfb311e3c673e4bdaec1fdfc38
cyj <cyd5538@naver.com>
2023-04-23 10:10:47 +0900

    2023-04-23

---
+++ 문자열/9935/app.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().split('\n');
+
+const arr = input[0].trim()
+const danger = input[1]
+
