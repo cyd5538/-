326ffe65e151df911c8f965880059c0507c4cc4b
cyj <cyd5538@naver.com>
2023-06-02 20:47:34 +0900

    2023-06-02

---
+++ 프로그래머스lv2/구명보트.js
@@ -0,0 +1,27 @@
+// 투포인터를 활용한 그리디 문제 해결
+// 투포인터는 정렬된 배열에서만 사용가능
+// 맨왼쪽과 맨끝쪽을 더해서 limit 보다 작으면 left++ 크면 right-- 
+// 같으면 answer++
+function solution(people, limit) {
+  let sorted = people.sort((a, b) => a - b);
+  
+  let left = 0;
+  let right = sorted.length - 1;
+  let answer = 0;
+
+  while (left <= right) {
+    if (sorted[left] + sorted[right] <= limit) {
+      left++;
+      right--;
+    } else {
+      right--;
+    }
+    answer++;
+    console.log(left,right)
+  }
+
+  return answer;
+}
+
+
+console.log(solution([70, 50, 80, 50],100));
